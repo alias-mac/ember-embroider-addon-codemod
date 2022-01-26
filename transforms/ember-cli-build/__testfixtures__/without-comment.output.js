0b913a3b5b78c7c647d7c59e117369d55c237cf4
Yang Piao <ypiao@linkedin.com>
2022-01-26 10:29:48 -0800

    update tmp-li-case to not always enable embroider

--- transforms/ember-cli-build/__testfixtures__/without-comment.output.js
+++ transforms/ember-cli-build/__testfixtures__/without-comment.output.js
@@ -13,6 +13,14 @@ module.exports = function (defaults) {
   const { compatAdapters } = require('@linkedin/pemberly-embroider/src');
   const adapters = compatAdapters();
 
+  // Making sure to use classic build when not running embroider test scenarios.
+  // maybeEmbroider turns on Embroider when @embroider/core is installed, and
+  // we have to install @embroider/core because it's a peerDep of
+  // @embroider/compat, which is a peerDep of @linkedin/pemberly-embroider.
+  if (!process.env.EMBROIDER_TEST_SETUP_OPTIONS) {
+    return app.toTree();
+  }
+
   return maybeEmbroider(app, {
     compatAdapters: new Map(adapters),
   });
